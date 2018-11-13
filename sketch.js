91d602e58aa606157213f69591b43fdcc8650313
IbraheemR <memyselfandi@ibraheemrodrigues.com>
2018-11-13 11:43:10 +0000

    This makes teh code better ;)

--- sketch.js
+++ sketch.js
@@ -4,6 +4,11 @@ function setup() {
 
 function draw() {
   background(random(0,255), random(0,255), random(0,255));
+  fill(0);
+  translate(width/2, height/2);
+  ellipse(0, 0, 50);
+  ellipse(0, 50, 50);
+  rect(0, 0, 50, 300);
 }
 
 function windowResized() {
