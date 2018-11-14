68c6216004138cd85820b46c2a546b3362a3242a
Blingboy740 <44981134+Blingboy740@users.noreply.github.com>
2018-11-14 13:41:35 +0000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,8 +6,8 @@ function draw() {
   background(255,255,100);
   fill(random(0,255),random(0,255),random(0,255));
   noStroke();
-  textSize(60);
-  text("EDDY AND BOG GAY",(windowWidth/2)-200,windowHeight/2);
+  textSize(windowWidth/10);
+  text("EDDY AND BOG GAY",(windowWidth/2)-300,windowHeight/2);
 }
 
 function windowResized() {
