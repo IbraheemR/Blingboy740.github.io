25d4c27086d24e36cc9cb49330f53c2699572d68
Blingboy740 <44981134+Blingboy740@users.noreply.github.com>
2018-11-14 13:30:21 +0000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,10 +5,7 @@ function setup() {
 function draw() {
   background(random(0,255), random(0,255), random(0,255));
   fill(0);
-  translate(width/2, height/2);
-  ellipse(0, 0, 50);
-  ellipse(0, 50, 50);
-  rect(0, 0, 50, 300);
+  rect(windowWidth/2,windowHeight/2,100,500)
 }
 
 function windowResized() {
